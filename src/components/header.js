513a206faf0d18892fefcc1d2f1c6110abbc89b5
dr-binaryV1 <developerdamian@gmail.com>
2017-07-01 11:57:19 -0500

    Added header styles and format header

--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,33 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 import { Link } from 'react-router-dom';
 
 class Header extends Component{
+    onSubmit(values){
+
+    }
+
     render(){
+        const { handleSubmit } = this.props;
+
         return(
             <div>
-                <h2>JusBuss Client Portal</h2>
-                <Link to="/">Home</Link>
-                <Link to="/Restaurants">Restaurants</Link>
+                <div className="header">
+                  <div className="logoContainer">
+                    <h2>JusBuss</h2>
+                  </div>
+                  <div className="searchBarContainer">
+                    <form onSubmit={ this.onSubmit.bind(this) }>
+                      <input
+                        className="searchBar"
+                        type="text" />
+                    </form>
+                  </div>
+                  <div className="navBarContainer">
+                    <Link to="/" className="nav-item">Home</Link>
+                    <Link to="/Restaurants" className="nav-item">Restaurants</Link>
+                  </div>
+                </div>
             </div>
         );
     }
