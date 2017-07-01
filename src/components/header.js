6e216fef152ac905f3e6ae2b328c9924b3d86d6e
dr-binaryV1 <developerdamian@gmail.com>
2017-07-01 12:11:32 -0500

    Fixed dangling code in header

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
 import { Field, reduxForm } from 'redux-form';
 import { Link } from 'react-router-dom';
 
@@ -29,24 +28,9 @@ class Header extends Component{
                     <Link to="/Restaurants" className="nav-item">Restaurants</Link>
                   </div>
                 </div>
-=======
-import { Link } from 'react-router-dom';
-
-class Header extends Component{
-    render(){
-        return(
-            <div>
-                <h2>JusBuss Client Portal</h2>
-                <Link to="/">Home</Link>
-                <Link to="/Restaurants">Restaurants</Link>
->>>>>>> 01667d91e8be39dc24a467d1b97b6a2ed1c98cef
             </div>
         );
     }
 }
 
-<<<<<<< HEAD
 export default Header;
-=======
-export default Header;
->>>>>>> 01667d91e8be39dc24a467d1b97b6a2ed1c98cef
