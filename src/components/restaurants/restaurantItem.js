211b904d8f36839d31725d5576e6ae7ab28b1522
dr-binaryV1 <developerdamian@gmail.com>
2017-07-02 15:53:22 -0500

    fix display issues on restaurants page, added information in display picture container

--- src/components/restaurants/restaurantItem.js
+++ src/components/restaurants/restaurantItem.js
@@ -6,12 +6,13 @@ const RestaurantItem = (props) => {
         <div className="restaurantItem" key={props.key}>
             <div className="DisplayImageContainer">
                 <img className="profile-picture" src={ props.Img } alt={ props.Alt } />
-            </div>
-            <div className="information">
-                <h4> { props.Name }</h4>
-                <hr className="line-brightPink-left" />
-                <p> { props.Desc } </p>
-                <Link to={ `/restaurants/${props.Id}` } className="blue-links">View Details</Link>
+            
+                <div className="information">
+                    <h4> { props.Name }</h4>
+                    <hr className="line-brightPink-left" />
+                    <p> { props.Desc } </p>
+                    <Link to={ `/restaurants/${props.Id}` } className="blue-links">View Details</Link>
+                </div>
             </div>
         </div>
     );
