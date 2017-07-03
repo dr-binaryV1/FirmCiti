98c8617b761329f306b19bf08ae8b99bd509e630
damian.wynter <damian.wynter@particularpresence.com>
2017-07-03 15:33:05 -0500

    combined comment reducer with other reducers

--- src/reducers/index.jsx
+++ src/reducers/index.jsx
@@ -3,6 +3,7 @@ import { reducer as formReducer } from 'redux-form';
 
 import UniversityReducer from './reducer_university';
 import RestaurantReducer from './reducer_restaurant';
+import CommentReducer from './reducer_comment';
 import LoginReducer from './reducer_login';
 import SignupReducer from './reducer_signup';
 
@@ -11,6 +12,7 @@ const rootReducer = combineReducers({
   restaurants: RestaurantReducer,
   loginStatus: LoginReducer,
   signupStatus: SignupReducer,
+  comment, CommentReducer,
   form: formReducer
 });
 
