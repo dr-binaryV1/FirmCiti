cb316898858543e0b7c67fe2873129f4a1416976
damian.wynter <damian.wynter@particularpresence.com>
2017-07-08 00:25:55 -0500

    added forms to add business' to database

---
+++ src/components/businessForms/addBusiness_as_Owner.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { reduxForm, Field } from 'redux-form';
+
+class AddBusinessAsOwner extends Component{
+    render(){
+        return(
+            <div></div>
+        );
+    }
+}
+
+export default AddBusinessAsOwner;
