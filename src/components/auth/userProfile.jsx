328579c9d99296bc6ffdcd02dd1420bb4dd79f92
damian.wynter <damian.wynter@particularpresence.com>
2017-07-09 17:04:50 -0500

    added function to read file and change image on page

--- src/components/auth/userProfile.jsx
+++ src/components/auth/userProfile.jsx
@@ -3,33 +3,57 @@ import { connect } from 'react-redux';
 import { Link, withRouter } from 'react-router-dom';
 
 class UserProfile extends Component {
-    onClick(){
+    constructor(props){
+        super(props);
 
+        this.state = ({
+            file: '',
+            imagePreviewUrl: ''
+        });
     }
 
-    handlechange(){
-      var fileinput = document.getElementById("upload");
-      var textinput = document.getElementById("filename");
-      textinput.value = fileinput.value;
+    _handleSubmit(e){
+        e.preventDefault();
+        console.log('handle uoloading...', this.state.file);
     }
 
-    handleUploadClick(){
-      var fileinput = document.getElementById("upload");
-      fileinput.click();
+    _handleImageChange(e){
+        e.preventDefault();
+
+        let reader = new FileReader();
+        let file = e.target.files[0];
+
+        reader.onloadend = () => {
+            this.setState({
+                file: file,
+                imagePreviewUrl: reader.result
+            });
+        }
+        reader.readAsDataURL(file);
     }
 
     render(){
         const { session } = this.props;
+        let { imagePreviewUrl } = this.state;
+        let $imagePreview = null;
+        if(imagePreviewUrl) {
+            $imagePreview = (<img className="profile-picture" src={ imagePreviewUrl } />);
+        } else {
+            $imagePreview = (<img className="profile-picture" src="/../src/static/images/noImage.jpg" alt="" />)
+        }
         return(
             <div className="profileContainer">
                 <div className="basic-info">
                     <div className="user-photo left">
-                        <img className="profile-picture" src="/../src/static/images/noImage.jpg" alt="" />
-                        <input type="file" id="upload" name="fileupload" onChange={ this.handlechange }/>
-                        <input type="text" id="filename"/>
-                        <input type="button" value="Upload" id="fakeBrowse" onClick={ this.handleUploadClick }/>
+                        { $imagePreview }
+
+                        <form onSubmit={(e) => this._handleSubmit(e)}>
+                            <input type="file" onChange={(e) => this._handleImageChange(e)} />
+                            <button type="submit" onClick={(e) => this._handleSubmit(e)}>upload</button>
+                        </form>
                         <button>edit profile</button>
                     </div>
+
                     <div className="user-info left">
                         <h4>{ `${session.user.first_name} ${session.user.last_name} (${session.user.username})` }</h4>
                         <hr />
