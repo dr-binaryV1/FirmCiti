6483acf6cc2e88176a12a17c9a0dae861b9ef1c9
dr-binaryV1 <developerdamian@gmail.com>
2017-07-14 23:50:29 -0500

    fix component margin

--- src/components/auth/signout.jsx
+++ src/components/auth/signout.jsx
@@ -11,16 +11,18 @@ class SignOut extends Component{
         sessionService.deleteSession();
         sessionService.deleteUser();
     }
-    
+
     render(){
         return(
             <div className="animated fadeIn signout-container">
-                <div className="signout-img">
-                    <p><img className="signed-out-img" src="src/static/images/signedOut.jpg" alt="signout image" /></p>
-                </div>
-                <div className="signout-msg">
-                    <h3>You just signed out. Please visit again</h3>
-                    <p>To sign in again, please click <Link to="/authorize">here</Link>.</p>
+                <div className="signout-content">
+                    <div className="signout-img">
+                        <p><img className="signed-out-img" src="src/static/images/signedOut.jpg" alt="signout image" /></p>
+                    </div>
+                    <div className="signout-msg">
+                        <h3>You just signed out. Please visit again</h3>
+                        <p>To sign in again, please click <Link to="/authorize">here</Link>.</p>
+                    </div>
                 </div>
             </div>
         );
