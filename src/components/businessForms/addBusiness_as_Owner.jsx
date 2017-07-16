035f04a8eea87549faf6f09114b727e1fd76ac2a
dr-binaryV1 <developerdamian@gmail.com>
2017-07-16 12:18:13 -0500

    added fields for add business form for business owners

--- src/components/businessForms/addBusiness_as_Owner.jsx
+++ src/components/businessForms/addBusiness_as_Owner.jsx
@@ -2,11 +2,103 @@ import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
 
 class AddBusinessAsOwner extends Component{
+    onSubmit(){
+            
+    }
+
+    renderField(field){
+        const { meta: { touched, error } } = field;
+        const className = `form-group ${touched && error ? 'has-danger' : ''}`;
+        
+        if(field.type !== 'textarea'){
+            return(
+                <div className = { className }>
+                    <label>{ field.label }</label>
+                    <input
+                        className="form-control"
+                        type={ field.type }
+                        { ...field.input } />
+                    <div className="text-help">
+                        { touched ? error : '' }
+                    </div>
+                </div>
+            );
+        }
+
+        if(field.type === 'textarea'){
+            return(
+                <div className = { className }>
+                    <label>{ field.label }</label>
+                    <textarea
+                        className="form-control"
+                        rows="2"
+                        { ...field.input }></textarea>
+                    <div className="text-help">
+                        { touched ? error : '' }
+                    </div>
+                </div>
+            );
+        }
+    }
+    
     render(){
+        const { handleSubmit } = this.props;
+
         return(
-            <div></div>
+            <div className="Container animated fadeIn">
+                <div className="add-business">
+                    <h3>Add your Business</h3>
+                    <hr className="line-brightPink-left" />
+                    <form onSubmit = { handleSubmit(this.onSubmit.bind(this)) }>
+                        <Field
+                            label="Name of Business:"
+                            name="name"
+                            type="text"
+                            component={ this.renderField } />
+
+                        <Field
+                            label="Description:"
+                            name="description"
+                            type="textarea"
+                            component={ this.renderField } />
+
+                        <Field
+                            label="Address:"
+                            name="address"
+                            type="textarea"
+                            component={ this.renderField } />
+
+                        <Field
+                            label="Telephone:"
+                            name="telephone"
+                            type="tel"
+                            component={ this.renderField } />
+
+                        <Field
+                            label="Open Time:"
+                            name="open_time"
+                            type="number"
+                            component={ this.renderField } />
+
+                        <Field
+                            label="Close Time:"
+                            name="close_time"
+                            type="number"
+                            component={ this.renderField } />
+
+                        <button className="btn btn-primary btn-sm" type="submit">Add Business</button>
+                    </form>
+                </div>
+            </div>
         );
     }
 }
 
-export default AddBusinessAsOwner;
+function validate(values){
+
+}
+
+export default reduxForm({
+    form: 'AddBusiness',
+    validate
+}) (AddBusinessAsOwner);
