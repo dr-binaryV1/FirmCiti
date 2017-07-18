9b5f6ff389189cf654b13cf440af0836761eb00d
dr-binaryV1 <developerdamian@gmail.com>
2017-07-17 20:25:17 -0500

    added cost calculation to menu item component

--- src/components/restaurants/restaurantMenuItem.jsx
+++ src/components/restaurants/restaurantMenuItem.jsx
@@ -2,13 +2,40 @@ import React, { Component } from 'react';
 
 //TODO Make Responsive
 class MenuItem extends Component {
+    componentDidUpdate(){
+
+    }
+
     renderVariations(){
         let sizeSelect = document.getElementById(`${this.props.Id}_size`);
         if(sizeSelect){
-            sizeSelect.innerHTML = "";
+            sizeSelect.innerHTML = "<option>Select Size</option>";
             return _.map(this.props.variations, variant => {
-                sizeSelect.innerHTML += "<option id="+ variant._id + ">"+ variant.size +"</option>";
+                sizeSelect.innerHTML += "<option value="+ variant.price +" id="+ variant._id + ">"+ variant.size +"</option>";
             });
+        }
+    }
+
+    sizeChanged(e){
+        let price = e.target.options[e.target.selectedIndex].value;
+        let priceLabel = document.getElementById(`${this.props.Id}_price`);
+        let qty = document.getElementById(`${this.props.Id}_qty`);
+        
+        priceLabel.innerHTML= `$${price}`;
+        qty.value = 1;
+    }
+
+    quantityChanged(){
+        let qty = document.getElementById(`${this.props.Id}_qty`).value;
+        let sizes = document.getElementById(`${this.props.Id}_size`);
+        let cost = sizes.options[sizes.selectedIndex].value;
+
+        if(parseInt(qty) < 0) {
+            document.getElementById(`${this.props.Id}_qty`).value = 0;
+        }
+
+        if(cost !== "Select Size" && parseInt(qty) > 0){
+            document.getElementById(`${this.props.Id}_price`).innerHTML = `$${cost * qty}`;
         }
     }
     
@@ -23,14 +50,14 @@ class MenuItem extends Component {
                     <p>Item: { this.props.name }</p>
                     <p>Description: { this.props.description }</p>
                     <label>Meal sizes:</label>
-                    <select id={ `${this.props.Id}_size` } className="form-control"></select>
+                    <select onChange={ (e) => this.sizeChanged(e) } id={ `${this.props.Id}_size` } className="form-control"></select>
                     { this.renderVariations() }
                 </div>
 
                 <div className="menu-price-info">
-                    <span> QTY: <input type="number" className="qty-input" id={ `${this.props.Id}-variant` } name="quantity" /></span>
-                    <h3 className="right">--N/A--</h3>
-                    <button className="btn btn-success btn-sm right">Add to Cart</button>
+                    <span> QTY: <input onChange={ this.quantityChanged.bind(this) } type="number" className="qty-input" id={ `${this.props.Id}_qty` } name="quantity" /></span>
+                    <h3 className="right-clear price" id={ `${this.props.Id}_price` }>--N/A--</h3>
+                    <button className="btn btn-success btn-sm right-clear">Add to Cart</button>
                 </div>
             </div>
         );
